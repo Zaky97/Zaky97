dbe2d6f010ced6b5fd403b547b8cda92d1e5753b
Zaky97 <zaky7645@gmail.com>
2024-02-12 10:24:03 +0700

    update cookie.js and firebase.js

--- js/cookie.js
+++ js/cookie.js
@@ -1,3 +1,62 @@
+const _0x370ca0 = _0x5427;
+(function (_0x47b2d3, _0x3afa05) {
+  const _0xaea09a = _0x5427,
+    _0x1df7c4 = _0x47b2d3();
+  while (!![]) {
+    try {
+      const _0x5d0b38 =
+        (-parseInt(_0xaea09a(0x96)) / 0x1) *
+          (-parseInt(_0xaea09a(0x8c)) / 0x2) +
+        -parseInt(_0xaea09a(0x87)) / 0x3 +
+        (-parseInt(_0xaea09a(0x82)) / 0x4) *
+          (-parseInt(_0xaea09a(0x99)) / 0x5) +
+        (-parseInt(_0xaea09a(0x8d)) / 0x6) * (parseInt(_0xaea09a(0x95)) / 0x7) +
+        parseInt(_0xaea09a(0x92)) / 0x8 +
+        (parseInt(_0xaea09a(0x7f)) / 0x9) * (-parseInt(_0xaea09a(0x88)) / 0xa) +
+        parseInt(_0xaea09a(0x89)) / 0xb;
+      if (_0x5d0b38 === _0x3afa05) break;
+      else _0x1df7c4["push"](_0x1df7c4["shift"]());
+    } catch (_0xef2af1) {
+      _0x1df7c4["push"](_0x1df7c4["shift"]());
+    }
+  }
+})(_0x2f06, 0x3370e);
+function _0x2f06() {
+  const _0x1f7b14 = [
+    "4581hAuGDE",
+    "addEventListener",
+    "location",
+    "184824AtWFTl",
+    "getItem",
+    "forEach",
+    "split",
+    "setItem",
+    "825108ynEhGQ",
+    "770pHYeiK",
+    "3443066KAixJe",
+    "login.html",
+    "firebaseToken",
+    "574946vhjWZn",
+    "6UxLUVN",
+    "querySelectorAll",
+    "pop",
+    "shift",
+    "DOMContentLoaded",
+    "756496jIiCDh",
+    "cookie",
+    "value",
+    "1837654aBUgdV",
+    "1bkIQLP",
+    "replace",
+    "input",
+    "10vBWOWs",
+    "length",
+  ];
+  _0x2f06 = function () {
+    return _0x1f7b14;
+  };
+  return _0x2f06();
+}
 import {
   app,
   database,
@@ -12,36 +71,41 @@ import {
   signInWithEmailAndPassword,
   fetchSignInMethodsForEmail,
 } from "./firebase.js";
-
-// Function to get cookie by name
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
+function _0x5427(_0x5e074c, _0x8ac2af) {
+  const _0x2f0621 = _0x2f06();
+  return (
+    (_0x5427 = function (_0x542773, _0xbb2a5) {
+      _0x542773 = _0x542773 - 0x7f;
+      let _0xdc6682 = _0x2f0621[_0x542773];
+      return _0xdc6682;
+    }),
+    _0x5427(_0x5e074c, _0x8ac2af)
+  );
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  // If Firebase token exists, redirect to main page
-  const firebaseToken = getCookie("firebaseToken");
-  if (!firebaseToken) {
-    window.location.replace("login.html");
-  }
-
-  // Mendapatkan semua elemen input
-  const inputElements = document.querySelectorAll("input");
-
-  // Loop melalui setiap elemen input
-  inputElements.forEach(function (input) {
-    // Mengecek apakah ada nilai yang disimpan di localStorage
-    const savedValue = localStorage.getItem(input.id);
-    if (savedValue) {
-      // Mengatur nilai input ke nilai yang disimpan di localStorage
-      input.value = savedValue;
-    }
-
-    // Menyimpan nilai setiap kali input berubah
-    input.addEventListener("input", function () {
-      localStorage.setItem(input.id, input.value);
-    });
+function getCookie(_0x203756) {
+  const _0x2627e0 = _0x5427,
+    _0x5058b3 = ";\x20" + document[_0x2627e0(0x93)],
+    _0x323e36 = _0x5058b3[_0x2627e0(0x85)](";\x20" + _0x203756 + "=");
+  if (_0x323e36[_0x2627e0(0x9a)] === 0x2)
+    return _0x323e36[_0x2627e0(0x8f)]()
+      [_0x2627e0(0x85)](";")
+      [_0x2627e0(0x90)]();
+}
+document[_0x370ca0(0x80)](_0x370ca0(0x91), function () {
+  const _0x2fc6d2 = _0x370ca0,
+    _0x15a2a9 = getCookie(_0x2fc6d2(0x8b));
+  !_0x15a2a9 && window[_0x2fc6d2(0x81)][_0x2fc6d2(0x97)](_0x2fc6d2(0x8a));
+  const _0x48bf1e = document[_0x2fc6d2(0x8e)](_0x2fc6d2(0x98));
+  _0x48bf1e[_0x2fc6d2(0x84)](function (_0x288909) {
+    const _0x3ff441 = _0x2fc6d2,
+      _0x23fc2d = localStorage[_0x3ff441(0x83)](_0x288909["id"]);
+    _0x23fc2d && (_0x288909["value"] = _0x23fc2d),
+      _0x288909[_0x3ff441(0x80)](_0x3ff441(0x98), function () {
+        const _0x27ddf6 = _0x3ff441;
+        localStorage[_0x27ddf6(0x86)](
+          _0x288909["id"],
+          _0x288909[_0x27ddf6(0x94)]
+        );
+      });
   });
 });
--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,51 @@
+const _0x2ca383 = _0x59ed;
+function _0x1e0b() {
+  const _0x17336b = [
+    "265741421264",
+    "4ZSGYpI",
+    "1:265741421264:web:5d6af087a996b58a9cecd5",
+    "1156435CRtElC",
+    "12258TqqBKE",
+    "1067605wfVbAF",
+    "157371Mltowj",
+    "2664ZoKHvr",
+    "9111vjyhEB",
+    "166730ItpCEG",
+    "386EFDxFi",
+    "836Ljwobd",
+    "954690QnpJzu",
+    "AIzaSyAiA-CPBjHgdNMSkECYJ3D55S-OJWeRpNM",
+    "https://noir-b0cf8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "noir-b0cf8.appspot.com",
+  ];
+  _0x1e0b = function () {
+    return _0x17336b;
+  };
+  return _0x1e0b();
+}
+(function (_0x360d0d, _0x4d887d) {
+  const _0x5d2050 = _0x59ed,
+    _0xe40627 = _0x360d0d();
+  while (!![]) {
+    try {
+      const _0x3b7756 =
+        -parseInt(_0x5d2050(0x175)) / 0x1 +
+        (parseInt(_0x5d2050(0x179)) / 0x2) *
+          (-parseInt(_0x5d2050(0x177)) / 0x3) +
+        (-parseInt(_0x5d2050(0x170)) / 0x4) *
+          (-parseInt(_0x5d2050(0x172)) / 0x5) +
+        parseInt(_0x5d2050(0x17b)) / 0x6 +
+        -parseInt(_0x5d2050(0x174)) / 0x7 +
+        (parseInt(_0x5d2050(0x176)) / 0x8) *
+          (-parseInt(_0x5d2050(0x173)) / 0x9) +
+        (parseInt(_0x5d2050(0x178)) / 0xa) * (parseInt(_0x5d2050(0x17a)) / 0xb);
+      if (_0x3b7756 === _0x4d887d) break;
+      else _0xe40627["push"](_0xe40627["shift"]());
+    } catch (_0x327bdf) {
+      _0xe40627["push"](_0xe40627["shift"]());
+    }
+  }
+})(_0x1e0b, 0x4b309);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
   getDatabase,
@@ -14,24 +62,30 @@ import {
   signInWithEmailAndPassword,
   fetchSignInMethodsForEmail,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAiA-CPBjHgdNMSkECYJ3D55S-OJWeRpNM",
-  authDomain: "noir-b0cf8.firebaseapp.com",
-  databaseURL:
-    "https://noir-b0cf8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "noir-b0cf8",
-  storageBucket: "noir-b0cf8.appspot.com",
-  messagingSenderId: "265741421264",
-  appId: "1:265741421264:web:5d6af087a996b58a9cecd5",
-  measurementId: "G-CDZKCSXS56",
-};
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app); // Initialize Firebase Auth
-
+    apiKey: _0x2ca383(0x17c),
+    authDomain: "noir-b0cf8.firebaseapp.com",
+    databaseURL: _0x2ca383(0x17d),
+    projectId: "noir-b0cf8",
+    storageBucket: _0x2ca383(0x16e),
+    messagingSenderId: _0x2ca383(0x16f),
+    appId: _0x2ca383(0x171),
+    measurementId: "G-CDZKCSXS56",
+  },
+  app = initializeApp(firebaseConfig),
+  database = getDatabase(app),
+  auth = getAuth(app);
+function _0x59ed(_0x3897ce, _0x4c2579) {
+  const _0x1e0b69 = _0x1e0b();
+  return (
+    (_0x59ed = function (_0x59ed78, _0x58443c) {
+      _0x59ed78 = _0x59ed78 - 0x16e;
+      let _0x342dce = _0x1e0b69[_0x59ed78];
+      return _0x342dce;
+    }),
+    _0x59ed(_0x3897ce, _0x4c2579)
+  );
+}
 export {
   app,
   database,
