575f21a719db4df7fd56af678bfdc8bac9fbcd3d
Muhammad Zaky <131351380+Zaky97@users.noreply.github.com>
2024-02-12 09:45:18 +0700

    update

--- js/login-animation.js
+++ js/login-animation.js
@@ -1,15 +1,15 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+// const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const lupaPassword = document.getElementById("lupapw");
 const animationDuration = 800;
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-  setTimeout(() => {
-    lupaPassword.style.display = "none";
-  }, animationDuration);
-});
+// sign_up_btn.addEventListener("click", () => {
+//   container.classList.add("sign-up-mode");
+//   setTimeout(() => {
+//     lupaPassword.style.display = "none";
+//   }, animationDuration);
+// });
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
