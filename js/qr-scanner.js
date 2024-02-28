4b3995b6e2e8d220a46857f4f561191387f2e8f8
Muhammad Zaky <131351380+Zaky97@users.noreply.github.com>
2024-02-28 04:26:38 +0000

    Update

--- js/qr-scanner.js
+++ js/qr-scanner.js
@@ -1,24 +1,24 @@
-  function domReady(fn) {
-      if (
-          document.readyState === "complete" ||
-          document.readyState === "interactive"
-      ) {
-          setTimeout(fn, 1000);
-      } else {
-          document.addEventListener("DOMContentLoaded", fn);
-      }
-  }
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
-  domReady(function () {
-      // If found you qr code
-      function onScanSuccess(decodeText, decodeResult) {
-          alert("You Qr is : " + decodeText, decodeResult);
-          window.location.href = decodeText;
-      }
+domReady(function () {
+    // If found your qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("Your QR is: " + decodeText);
+        window.location.href = decodeText;
+    }
 
-      let htmlscanner = new Html5QrcodeScanner(
-          "reader",
-          { fps: 10, qrbos: 250 }
-      );
-      htmlscanner.render(onScanSuccess);
-  });
+    let htmlscanner = new Html5QrcodeScanner(
+        "reader", // No HTML element to render the scanner
+        { fps: 10, qrbox: 250, preferFrontCamera: false }
+    );
+    htmlscanner.render(onScanSuccess);
+});
